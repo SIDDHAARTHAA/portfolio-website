a8f83292fab39b4a64b832eac079ff6abbf237b6
SIDDHAARTHAA <siddhaartha09@gmail.com>
2025-07-19 11:41:59 +0000

    docker setup -without icons -with style

---
+++ app/web2/page.tsx
@@ -0,0 +1,12 @@
+// "use client"
+import DockBar from "../components/DockBar";
+// import ThemeToggle from "../components/ThemeToggle";
+
+export default function Web2() {
+    return (
+        <div>
+            <h1>Hello form web2</h1>
+            <DockBar />
+        </div>
+    )
+}
