fef0f627cf8153b1bb28c34b9b07aa3fddd89a0f
Nisarga B S <nisarga@Nisargas-MacBook-Air.local>
2025-07-29 17:03:09 +0530

    view pdf

--- app/components/HeroButtons.tsx
+++ app/components/HeroButtons.tsx
@@ -9,7 +9,11 @@ export default function HeroButtons() {
     return (
         <div className="flex gap-2">
             <button className="border cursor-pointer px-4 py-2 rounded-md transition-colors ease-out duration-500" onClick={() => scrollToSection('aboutme')}>About Me</button>
-            <button className="border cursor-pointer px-4 py-2 rounded-md transition-colors ease-out duration-500">Download CV</button>
+            {/* <button className="border cursor-pointer px-4 py-2 rounded-md transition-colors ease-out duration-500">Download CV</button> */}
+
+            <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+                <button className="border cursor-pointer px-4 py-2 rounded-md transition-colors ease-out duration-500">View CV</button>
+            </a>
         </div>
     )
 }
