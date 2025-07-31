0398ef0166763eeac2688bfabec17260e605ade3
Nisarga B S <siddhaartha09@gmail.com>
2025-07-31 20:58:32 +0530

    spotlight effect on hero card

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,12 +1,15 @@
+'use client'
 import Image from "next/image";
 import myImage from '../../public/myImage.jpg';
 import HeroButtons from "./HeroButtons";
+import SpotlightCard from '@/reactBits/blocks/Components/SpotlightCard/SpotlightCard';
 
 export default function Hero() {
     return (
         <div className="min-h-screen flex flex-col items-center relative px-4">
-            <div className="flex flex-col xl:flex-row items-center w-[350px] h-[600px] md:w-[700px] xl:w-[750px] xl:h-auto justify-center border-1 border-card-border rounded-xl p-6 md:p-8 gap-6 xl:gap-8 shadow-xl/10 mt-0 xl:mt-25">
+            <SpotlightCard className="custom-spotlight-card flex flex-col xl:flex-row items-center w-[350px] h-[600px] md:w-[700px] xl:w-[750px] xl:h-auto justify-center border-1 border-card-border rounded-xl p-6 md:p-8 gap-6 xl:gap-8 shadow-xl/10 mt-0 xl:mt-25" spotlightColor="rgba(234, 152, 1, 0.4)">
                 <div className="flex-shrink-0 border border-card-border rounded-full">
+                     {/* rgba(174, 184, 255, 1) */}
                     <Image
                         src={myImage}
                         alt="image"
@@ -30,7 +33,7 @@ export default function Hero() {
                         <HeroButtons />
                     </div>
                 </div>
-            </div>
+        </SpotlightCard>
         </div>
     )
 }
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,6 +1,8 @@
+'use client'
 import project1Img from '@/public/myImage.jpg'
 import { px } from 'framer-motion'
 import Image from 'next/image'
+import ScrollStack, { ScrollStackItem } from '@/reactBits/blocks/Components/ScrollStack/ScrollStack'
 export default function Projects() {
     const projects = [
         {
@@ -10,22 +12,8 @@ export default function Projects() {
         }
     ]
     return (
-        <div className="min-h-screen px-4 flex flex-col items-center gap-8 mt-6 mb-20">
-            <div className='border border-card-border p-4 rounded-2xl gap-4'>
-                {projects.map((project, i) => (
-                    <div key={i} className=''>
-                        <h1>
-                            {project.title}
-                        </h1>
-                        <div className='flex text-pretty'>
-                            <Image src={project.image} alt={project.title} width={250} className='rounded-2xl' />
-                            <p className='text-base md:text leading-relaxed'>
-                                {project.description}
-                            </p>
-                        </div>
-                    </div>
-                ))}
-            </div>
+        <div className="">
+            Tasks
         </div>
     )
 }
