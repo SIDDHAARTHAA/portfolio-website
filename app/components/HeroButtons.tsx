03a5d9fe25cee22422789425110e03895bb3d833
Nisarga B S <siddhaartha09@gmail.com>
2025-08-14 19:52:27 +0530

    update: resume viewer

--- app/components/HeroButtons.tsx
+++ app/components/HeroButtons.tsx
@@ -9,8 +9,7 @@ export default function HeroButtons() {
     return (
         <div className="flex gap-2">
             <button className="border cursor-pointer px-4 py-2 rounded-md transition-colors ease-out duration-500" onClick={() => scrollToSection('aboutme')}>About Me</button>
-            {/* <button className="border cursor-pointer px-4 py-2 rounded-md transition-colors ease-out duration-500">Download CV</button> */}
-            <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+            <a href="https://drive.google.com/file/d/1_lqL2yHO3GfUxJgemYqgtevdZel9IBrv/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                 <button className="border cursor-pointer px-4 py-2 rounded-md transition-colors ease-out duration-500">View Resume</button>
             </a>
         </div>
